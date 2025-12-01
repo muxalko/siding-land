099ce39c8709f99c55e9a6391fd4deb0b8f99b7b
Muxalko <muxa.baba@gmail.com>
2025-12-02 01:27:26 +0200

    bump vite vrsion

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+// import { componentTagger } from "lovable-tagger";
+import vercel from 'vite-plugin-vercel';
+ 
+// export default defineConfig({
+//   server: {
+//     port: process.env.PORT as unknown as number,
+//   },
+//   plugins: [vercel()],
+// });
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
-    port: 8080,
+    // host: "::",
+    // port: 8080,
+    port: process.env.PORT as unknown as number,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugin: [react(), vercel()].filter(Boolean),
+  // plugins: [react(), mode === "development" && componentTagger(), vercel()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
